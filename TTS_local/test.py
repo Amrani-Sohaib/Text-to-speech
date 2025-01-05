e51a8b0ceaf38096e1fe92dc860221e208d543ee
Amrani-Sohaib <Souhailnoragami@gmail.com>
2025-01-05 01:22:47 +0100

    convert isolation

--- TTS_local/test.py
+++ TTS_local/test.py
@@ -7,54 +7,27 @@ import subprocess
 # Get device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
-
-# List available TTS models
+# List available TTS 
 
 # Init TTS
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", progress_bar=False) 
+#tts = TTS("tts_models/en/ljspeech/glow-tts", progress_bar=False) 
+tts = tts.to(device)
 
 content = "  يمثل إيرين ييغر شخصية معقدة في علاقتها بالفلسفة العدمية. فبعد اكتشافه للحقيقة المريرة حول عالمه والتاريخ المأساوي لشعبه، يتحول تفكيره تدريجياً نحو رؤية عدمية للوجود. يرى أن المعاناة والصراع هما جوهر الحياة، وأن البحث عن معنى أو قيم مطلقة هو وهم. هذا التحول الفلسفي يدفعه لاتخاذ قرارات متطرفة، معتقداً أن الحرية الحقيقية تكمن في القدرة على تدمير العالم وإعادة تشكيله، حتى لو كان ذلك يعني التخلي عن إنسانيته وقيمه السابقة."
 
-content_fr = "bonjour je suis amrani sohaib et je suis un étudiant en informatique à l'université de constantine 2, je suis passionné par l'intelligence artificielle et le machine learning, j'ai réalisé plusieurs projets en utilisant ces technologies, je suis très content de travailler avec vous et j'espère que je vais apprendre beaucoup de choses avec vous, merci."
+content_fr = "bonjour je suis amrani sohaib et je suis un étudiant en informatique à l'université de constantine 2, je suis passionné par l'intelligence artificielle et le machine learning. j'ai réalisé plusieurs projets en utilisant ces technologies. je suis très content de travailler avec vous et j'espère que je vais apprendre beaucoup de choses avec vous merci"
+content_eng = 'hello my name is sohaib amrani and i am a computer science student at the university of constantine 2, i am passionate about artificial intelligence and machine learning. i have completed several projects using these technologies. i am very happy to work with you and i hope that i will learn a lot of things with you, thank you'
 # Run TTS
 # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
 # Text to speech list of amplitude values as output
-"""
 
 
-
-"""
-# Directory containing .mp3 files
-segments_dir = r"C:\Users\Sohaib-\RepoML\Text-to-speech\audios\segments"
-
-# Directory to save converted .wav files
-output_dir = os.path.join(segments_dir, "converted_wavs")
-os.makedirs(output_dir, exist_ok=True)
-
-# Function to convert .mp3 to .wav using ffmpeg
-def convert_mp3_to_wav(mp3_path, wav_path):
-    try:
-        # Run ffmpeg command
-        subprocess.run(
-            ["ffmpeg", "-i", mp3_path, wav_path, "-y"],
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
-        print(f"Converted {mp3_path} to {wav_path}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error converting {mp3_path}: {e.stderr.decode()}")
-
-# Convert all .mp3 files in the directory
-for file in os.listdir(segments_dir):
-    if file.endswith(".mp3"):
-        mp3_path = os.path.join(segments_dir, file)
-        wav_path = os.path.join(output_dir, os.path.splitext(file)[0] + ".wav")
-        convert_mp3_to_wav(mp3_path, wav_path)
-
+waves_path = r"C:\Users\Sohaib-\RepoML\Text-to-speech\audios\converted_wavs"
 # List all .wav files in the converted directory
-speaker_wavs = [os.path.join(output_dir, file) for file in os.listdir(output_dir) if file.endswith(".wav")]
-print(f"Speaker .wav files: {speaker_wavs}")
+speaker_wavs = [os.path.join(waves_path, file) for file in os.listdir(waves_path) if file.endswith(".wav")]
+#print(f"Speaker .wav files: {speaker_wavs}")
+
 
 
 
@@ -70,8 +43,8 @@ wav = tts.tts(text=content,
               )
 """
 
-
-tts.tts_to_file(text=content_fr, 
+tts.tts_to_file(text=content_eng, 
                 file_path="output.wav",
-                speaker_wav=speaker_wavs, 
-                language="fr" )
+                speaker_wav=speaker_wavs,
+                language="en" 
+                )
